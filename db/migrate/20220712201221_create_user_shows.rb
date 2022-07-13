e7d52ac27df80fa32586e59882b48a86da14b31a
B(ritta)ny <102260011+b-ritta-ny@users.noreply.github.com>
2022-07-13 13:00:48 -0400

    added migrations

--- db/migrate/20220712201221_create_user_shows.rb
+++ db/migrate/20220712201221_create_user_shows.rb
@@ -1,6 +1,8 @@
 class CreateUserShows < ActiveRecord::Migration[7.0]
   def change
     create_table :user_shows do |t|
+      t.integer :user_id
+      t.integer :show_id
       t.string :rating
       t.boolean :watched
 
